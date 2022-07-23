f4a173903155b553684e0c3377d03fd301853a76
Siam T. Bhuiyan <siamtbhuiyan@gmail.com>
2022-07-23 17:01:13 +0600

    Country Quiz PWA

--- Front-end Developer/country-quiz/src/components/Footer.vue
+++ Front-end Developer/country-quiz/src/components/Footer.vue
@@ -2,9 +2,19 @@
 <template>
   <div class="footer">
     created by
-    <a href="https://devchallenges.io/portfolio/siamtbhuiyan" class="font-bold"
-      >@siamtbhuiyan</a
+    <a
+      href="https://github.com/siamtbhuiyan"
+      class="font-bold px-1"
+      target="_blank"
     >
-    - devChallenges
+      @siamtbhuiyan</a
+    >
+    -
+    <a
+      href="https://devchallenges.io/solutions/1qlSE0egxnFk3dntCZOp"
+      class="px-1 cursor-pointer"
+      target="_blank"
+      >devChallenges</a
+    >
   </div>
 </template>
