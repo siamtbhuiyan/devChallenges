4f3f33be07135cbf19045a0387b089f1344eb621
Siam T. Bhuiyan <siamtbhuiyan@gmail.com>
2022-11-16 01:01:44 +0600

    Portfolio Page Layout

--- Responsive Web Developer/Portfolio/src/App.jsx
+++ Responsive Web Developer/Portfolio/src/App.jsx
@@ -2,7 +2,29 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className="font-bold text-3xl">Portfolio</div>
+    <div className="h-screen w-screen background p-6 font-montserrat">
+      <div className='container mx-auto grid grid-cols-5'>
+          <div className="profile">
+            PROFILE
+          </div>
+          <div className='skills'>
+            SKILLS
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit laudantium sequi commodi, eius cupiditate atque voluptate illum est voluptates quaerat ea officia, accusantium repellat possimus consequatur! Veritatis molestiae accusantium quidem?
+          </div>
+          <div className='blog'>
+            blog
+          </div>
+          <div className='hobbies'>
+            hobbies
+          </div>
+          <div className='learning'>
+            learning
+          </div>
+          <div className='projects'>
+            projects
+          </div>
+      </div>
+    </div>
   )
 }
 
