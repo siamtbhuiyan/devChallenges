7a742b7724deb795045fa982b45991b2439427c0
Siam T. Bhuiyan <siamtbhuiyan@gmail.com>
2023-02-07 22:24:43 +0600

    Portfolio Skills

--- Responsive Web Developer/Portfolio/src/components/Skills.jsx
+++ Responsive Web Developer/Portfolio/src/components/Skills.jsx
@@ -3,17 +3,42 @@ import React from 'react'
 const Skills = () => {
   return (
     <div className='skills'>
-        <div className='uppercase text-gray-800 font-bold text-lg'>front end</div>
+        <div className='uppercase text-gray-800 font-bold text-lg pb-2'>front end</div>
           <div className="bars">
-              <div class="bg-light relative h-[10px] w-full rounded-2xl">
-                <div
-                  class="bg-primary absolute top-0 left-0 h-full w-1/2 rounded-2xl"
-                ></div>
+            <div className='bar'>
+              <div className='flex-1 text-[#4F4F4F]'>React</div>
+              <div class="flex-[3] bg-gray-200 rounded-full h-2 dark:bg-gray-700 my-auto">
+                <div class="bg-blue-600 h-2 rounded-full w-2/3"></div>
               </div>
-            <div>
-              CSS
-              <div class="bg-gray-200 rounded-full h-2 dark:bg-gray-700">
-                <div class="bg-blue-600 h-2 rounded-full w-6/12"></div>
+            </div>
+            <div className='bar'>
+              <div className='flex-1 text-[#4F4F4F]'>Javascript</div>
+              <div class="flex-[3] bg-gray-200 rounded-full h-2 dark:bg-gray-700 my-auto">
+                <div class="bg-blue-600 h-2 rounded-full w-9/12"></div>
+              </div>
+            </div>
+            <div className='bar'>
+              <div className='flex-1 text-[#4F4F4F]'>CSS</div>
+              <div class="flex-[3] bg-gray-200 rounded-full h-2 dark:bg-gray-700 my-auto">
+                <div class="bg-blue-600 h-2 rounded-full w-11/12"></div>
+              </div>
+            </div>
+            <div className='bar'>
+              <div className='flex-1 text-[#4F4F4F]'>Vue</div>
+              <div class="flex-[3] bg-gray-200 rounded-full h-2 dark:bg-gray-700 my-auto">
+                <div class="bg-blue-600 h-2 rounded-full w-2/3"></div>
+              </div>
+            </div>
+            <div className='bar'>
+              <div className='flex-1 text-[#4F4F4F]'>Redux</div>
+              <div class="flex-[3] bg-gray-200 rounded-full h-2 dark:bg-gray-700 my-auto">
+                <div class="bg-blue-600 h-2 rounded-full w-9/12"></div>
+              </div>
+            </div>
+            <div className='bar'>
+              <div className='flex-1 text-[#4F4F4F]'>React Native</div>
+              <div class="flex-[3] bg-gray-200 rounded-full h-2 dark:bg-gray-700 my-auto">
+                <div class="bg-blue-600 h-2 rounded-full w-11/12"></div>
               </div>
             </div>
           </div>
