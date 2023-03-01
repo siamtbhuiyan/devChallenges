d96b93ef78697c2105d5e8d24243ada765920062
Siam T. Bhuiyan <siamtbhuiyan@gmail.com>
2023-03-01 22:23:35 +0600

    Search Jobs Search Functionality

--- Front-end Developer/search-jobs/src/App.jsx
+++ Front-end Developer/search-jobs/src/App.jsx
@@ -1,5 +1,5 @@
 import jobService from "./services/jobs";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import {
   BrowserRouter as Router,
   Routes, Route, Link
@@ -13,6 +13,9 @@ const App = () => {
   const [currentPage, setCurrentPage] = useState(1)
   const [totalPages, setTotalPages] = useState(1)
   const [isFullTime, setIsFullTime] = useState(false)
+  const [allJobs, setAllJobs] = useState(null)
+
+  const inputRef = useRef(null)
 
 
   const getAll = async () => {
@@ -23,6 +26,7 @@ const App = () => {
           page: Math.ceil((index + 1)/5)
         }
       })
+      setAllJobs(currentJobs)
       setJobs(currentJobs)  
       setTotalPages(Math.ceil(jobs.length/5))
     }
@@ -46,19 +50,11 @@ const App = () => {
     }
 
     const toggleFullTime = async () => {
+      setCurrentPage(1)
       setIsFullTime(!isFullTime)
       if (isFullTime) {
-        await jobService.getAll().then((jobs) => {
-          const currentJobs = jobs.map((j, index) => {
-            return {
-              ...j,
-              page: Math.ceil((index + 1)/5)
-            }
-          })
-          setJobs(currentJobs)  
-          setTotalPages(Math.ceil(jobs.length/5))
-        }
-        )
+        setJobs(allJobs)
+        setTotalPages(Math.ceil(allJobs.length/5))
       } else {
         await jobService.getFullTime().then((jobs) => {
           const currentJobs = jobs.map((j, index) => {
@@ -68,19 +64,47 @@ const App = () => {
             }
           })
           setJobs(currentJobs)  
-          setTotalPages(Math.ceil(jobs.length/5))
+          setTotalPages(Math.ceil(currentJobs.length/5))
         }
         )
-      }
-      
+      } 
     }
+
+    const searchJobs = async (searchField) => {
+      setCurrentPage(1)
+      const filteredJobs = allJobs.filter(
+        job => {
+          return (
+            job
+            .title
+            .toLowerCase()
+            .includes(searchField.toLowerCase()) ||
+            job
+            .name
+            .toLowerCase()
+            .includes(searchField.toLowerCase())
+          );
+        }
+      );
+      const currentJobs = filteredJobs.map((j, index) => {
+        return {
+          ...j,
+          page: Math.ceil((index + 1)/5)
+        }
+      })
+      setJobs(currentJobs)
+      console.log(2, jobs)
+      setTotalPages(Math.ceil(currentJobs.length/5))
+      console.log(totalPages)
+    }
+
 
 
   return (
       <div className="">
         <Router>
         <Routes>
-          <Route path="/" element={<Home jobs={jobs} page={currentPage} totalPages={totalPages} changePage={changePage} toggleFullTime={toggleFullTime} />}/>
+          <Route path="/" element={<Home jobs={jobs} page={currentPage} totalPages={totalPages} changePage={changePage} toggleFullTime={toggleFullTime} searchJobs={searchJobs} inputRef={inputRef} />}/>
           <Route path="/jobs" element={<Jobs />}/>
         </Routes>
       </Router>
--- Front-end Developer/search-jobs/src/components/Home.jsx
+++ Front-end Developer/search-jobs/src/components/Home.jsx
@@ -2,12 +2,12 @@ import Search from "./Search"
 import Filter from "./Filter"
 import JobsList from "./JobsList"
 
-const Home = ({ jobs, page, totalPages, changePage, toggleFullTime }) => {
+const Home = ({ jobs, page, totalPages, changePage, toggleFullTime, searchJobs, inputRef }) => {
     
   return (
     <div className="container mx-auto font-poppins">
         <div className="font-bold text-2xl my-8">Search Jobs</div>
-        <Search />
+        <Search searchJobs={searchJobs} inputRef={inputRef} />
         <div className="flex">
             <Filter toggleFullTime={toggleFullTime}/>
             <JobsList jobs={jobs} page={page} totalPages={totalPages} changePage={changePage} />
